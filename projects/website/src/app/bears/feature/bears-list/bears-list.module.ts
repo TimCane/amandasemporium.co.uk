b3826ee21bfb3291a40aa00cb2b367cf2fa6515c
Tim Cane <3074314+TimCane@users.noreply.github.com>
2023-07-04 10:57:24 +0100

    last nights changes

--- projects/website/src/app/bears/feature/bears-list/bears-list.module.ts
+++ projects/website/src/app/bears/feature/bears-list/bears-list.module.ts
@@ -6,6 +6,7 @@ import { BearsListRoutingModule } from './bears-list-routing.module';
 import { BearsListComponent } from './bears-list.component';
 
 import { AToZPaginationModule } from '../../../shared/components/a-to-z-pagination/a-to-z-pagination.module';
+import { PaginationModule } from '../../../shared/components/pagination/pagination.module';
 
 @NgModule({
   declarations: [BearsListComponent],
@@ -13,6 +14,7 @@ import { AToZPaginationModule } from '../../../shared/components/a-to-z-paginati
     CommonModule,
     BearsListRoutingModule,
     BearTileModule,
+    PaginationModule,
     AToZPaginationModule,
   ],
 })
