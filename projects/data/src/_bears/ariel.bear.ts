a4c662b7b96350f6d0a14a2bad2e02c31a1f95c8
Tim Cane <3074314+TimCane@users.noreply.github.com>
2023-07-06 21:51:27 +0100

    added more bear descriptions

--- projects/data/src/_bears/ariel.bear.ts
+++ projects/data/src/_bears/ariel.bear.ts
@@ -6,7 +6,12 @@ import { IBear } from '../lib/interfaces/bear.interface';
 export const ariel: IBear = {
   Id: 'ariel',
   Name: 'Ariel',
-  Description: ``,
+  Description: `Introducing Ariel, the enchanting teddy bear who finds joy and tranquility in the depths of the sea! With her shimmering fur, dreamy eyes, and a heart that longs for the ocean, Ariel is the perfect companion for those who are captivated by the mysteries and beauty of the sea.
+  Ariel's love for the sea is evident in every aspect of her being. She is drawn to the rhythmic waves, the soothing sounds of crashing surf, and the vastness of the ocean. Ariel's fur sparkles like ocean water under the sun, and her eyes reflect the depth and wonder of the underwater world.
+  As a friend, Ariel is gentle, compassionate, and always ready to listen to the stories the sea has to tell. She understands the healing power of the ocean's embrace and the sense of peace it can bring. Ariel's presence is like a refreshing sea breeze, comforting and invigorating at the same time.
+  Ariel's adventures are filled with exploration and discovery. She longs to swim alongside graceful dolphins, playfully chase colorful fish, and marvel at the vibrant coral reefs. Ariel inspires a sense of wonder and awe for the wonders that lie beneath the surface, reminding us to cherish the beauty of nature and to protect our oceans.
+  With Ariel by your side, every day becomes a seaside adventure. Whether you're imagining the crash of waves on a distant shore, collecting seashells on sandy beaches, or simply reveling in the serenity of a quiet moment by the sea, Ariel's love for the ocean will transport you to a world of tranquility and awe.
+  So, if you're seeking a furry friend who embodies the spirit of the sea and the magic of underwater worlds, Ariel the sea-loving teddy bear is the perfect choice. Get ready to dive into enchanting adventures, embrace the beauty of the ocean, and experience the peace and joy that Ariel brings as you navigate the depths of your own imagination.`,
   UploadedOn: new Date(2022, 3, 24),
   Type: {
     Type: BearTypeEnum.BuildABear,
--- projects/data/src/_bears/danny.bear.ts
+++ projects/data/src/_bears/danny.bear.ts
@@ -7,7 +7,13 @@ import { IBear } from '../lib/interfaces/bear.interface';
 export const danny: IBear = {
   Id: 'danny',
   Name: 'Danny',
-  Description: ``,
+  Description: `Introducing Danny, the adventurous teddy bear with a love for dinosaurs and a special place in his heart for the magnificent Triceratops! With his curious eyes, dinosaur-themed attire, and a heart filled with prehistoric wonder, Danny is the perfect companion for those who are fascinated by the ancient world and the mighty creatures that once roamed the Earth.
+  Danny's passion for dinosaurs is evident in every aspect of his appearance. From his dino-inspired shirt to his playful dinosaur-printed accessories, he proudly displays his love for these ancient giants. Danny's sparkling eyes reflect his sense of wonder and excitement, making him an enchanting friend for any aspiring paleontologist or dinosaur enthusiast.
+  As a friend, Danny is inquisitive, playful, and always ready for prehistoric adventures. He understands the importance of curiosity and the joy of discovery. Danny's presence sparks imagination and encourages others to embrace their own sense of wonder about the natural world.
+  Danny's dinosaur adventures are filled with thrilling expeditions and exciting discoveries. Whether he's imagining roaming the wild landscapes of the past, uncovering fossil treasures, or learning about different dinosaur species, he brings the ancient world to life. Danny's fascination with dinosaurs ignites a sense of awe and instills a love for learning and exploring.
+  While Danny may not be a real dinosaur, he carries the spirit of these majestic creatures and the desire to protect our natural heritage. He understands the importance of conservation and cherishing the wonders of the natural world. Danny's presence fosters a sense of responsibility and a commitment to preserving the legacy of these incredible creatures.
+  With Danny by your side, every day becomes an opportunity for exciting dinosaur adventures. Whether you're engaging in imaginative play, studying dinosaur facts, or simply appreciating the beauty of the natural world, Danny's love for dinosaurs will ignite your own sense of curiosity and deepen your connection to the prehistoric era.
+  So, if you're seeking a furry friend who embodies the spirit of dinosaur fascination and the joy of discovery, Danny the dinosaur-loving teddy bear is the perfect choice. Get ready to embark on epic prehistoric journeys, embrace the wonders of the dinosaur world, and experience the joy and excitement that Danny brings as you uncover the mysteries of the past together.`,
   UploadedOn: new Date(2019, 8, 9),
   Type: {
     Type: BearTypeEnum.BuildABear,
