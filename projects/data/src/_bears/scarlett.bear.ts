9a5a3de9d731823afcb926341bfb0ea496347a45
Tim Cane <3074314+TimCane@users.noreply.github.com>
2023-09-29 15:15:28 +0100

    updated home

--- projects/data/src/_bears/scarlett.bear.ts
+++ projects/data/src/_bears/scarlett.bear.ts
@@ -2,7 +2,7 @@ import { buildABear } from '../_bear-brands/build-a-bear.bear-brand';
 import { koala } from '../_bear-species/koala.bear-species';
 import { E20230923 } from '../_events/E2023-09-23.event';
 import { addlestone } from '../_locations/addlestone.location';
-import { surbiton } from '../_locations/surbiton.location';
+import { chessington } from '../_locations/chessington.location';
 import { IBear } from '../lib/interfaces/bear.interface';
 
 export const scarlett: IBear = {
@@ -20,7 +20,7 @@ export const scarlett: IBear = {
   },
   Event: E20230923,
   Rehomed: {
-    Location: surbiton,
+    Location: chessington,
     Date: new Date(2023, 8, 23),
   },
 };
