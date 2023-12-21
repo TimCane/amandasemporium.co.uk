1b467fd863cb846c510edfca7cc21c9714fb7868
Tim Cane <3074314+TimCane@users.noreply.github.com>
2023-12-21 12:48:19 +0000

    added lyla bear

---
+++ projects/data/src/_locations/omagh.location.ts
@@ -0,0 +1,8 @@
+import { ILocation } from '../lib/interfaces/location.interface';
+
+export const omagh: ILocation = {
+  Id: 'omagh',
+  Name: 'Omagh',
+  Latitude: 54.5977,
+  Longitude: 7.31,
+};
