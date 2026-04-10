8d90a5dfe83d37e97f3982b7503dc5ffa2b26d85
Tim Cane <3074314+TimCane@users.noreply.github.com>
2026-04-10 18:08:58 +0000

    Update project config and dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 import sitemap from '@astrojs/sitemap';
+import preact from '@astrojs/preact';
+import remarkDateTokens from './src/lib/remark-date-tokens.ts';
 
 export default defineConfig({
   adapter: node({ mode: 'standalone' }),
-  integrations: [sitemap()],
+  integrations: [sitemap(), preact()],
+  markdown: {
+    remarkPlugins: [remarkDateTokens],
+  },
   site: 'https://amandasemporium.co.uk',
   vite: {
     css: {
